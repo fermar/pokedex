4cf1143157afd6b97ef2b8b772be481fc9ca67be
fermar <fermar.ingenieria@gmail.com>
2025-02-04 16:45:28 +0000

    lesson 4

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+
+		fmt.Print("Pockedex > ")
+
+		scanner.Scan()
+		err := scanner.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		input := strings.Fields(strings.ToLower(scanner.Text()))
+		fmt.Printf("Your command was: %v \n", input[0])
+	}
 }
