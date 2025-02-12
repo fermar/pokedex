8c19843c45d0c0c3891048931b4a0f17e462d0b8
fermar <fermar.ingenieria@gmail.com>
2025-02-12 14:15:14 +0000

    se borra codigo comentado

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -28,25 +28,11 @@ func (cli *Client) ListLoc(pageURL *string) (LocAreaList, error) {
 	if err != nil {
 		return LocAreaList{}, err
 	}
-	// body, err := io.ReadAll(res.Body)
-	// res.Body.Close()
-	// if res.StatusCode > 299 {
-	// return nil, nil, err
-	// }
-	// if err != nil {
-	// return nil, nil, err
-	// }
-
 	locList := LocAreaList{}
 	err = json.Unmarshal(data, &locList)
 	if err != nil {
 		return LocAreaList{}, err
 	}
 	return locList, nil
-	// for _, result := range locList.Results {
-	// 	fmt.Println(result.Name)
-	// }
-	// fmt.Println()
-	//
-	// return locList.Next, locList.Previous, nil
+
 }
